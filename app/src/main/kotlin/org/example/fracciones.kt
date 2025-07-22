a0c90b8588bf7679bbb42fe844b4bdf8ab501e5a
mati diaz <j.ardevol@alumno.etec.um.edu.ar>
2025-07-22 12:47:25 -0300

    Etapa 1: Definición básica de la clase Fraccion

--- app/src/main/kotlin/org/example/fracciones.kt
+++ app/src/main/kotlin/org/example/fracciones.kt
@@ -1,61 +1,24 @@
-Fraccion.kt : 
 package org.example
 
+class Fraccion(private var _numerador: Int, private var _denominador: Int) {
 
-class Fraccion(
-   private var _numerador: Int,
-   private var _denominador: Int
-) {
+    var numerador: Int = _numerador
+        get() = field
+        set(value) { field = value }
 
+    var denominador: Int = _denominador
+        get() = field
+        set(value) { field = value
+        }
 
-   var numerador: Int
-       get() = _numerador
-       set(value) {
-           _numerador = value
-       }
+    init { if (denominador == 0){
+        throw IllegalArgumentException("El denominador no puede ser cero")}}
 
+    override fun toString(): String {
+        return ("$_numerador/$_denominador")
+    }
 
-   var denominador: Int
-       get() = _denominador
-       set(value) {
-           _denominador = value
-       }
-
-
-   operator fun plus(otra: Fraccion): Fraccion {
-       var nuevo_numerador = this._numerador * otra.
-       _denominador + this._denominador * otra._numerador
-       var nuevo_denominador = this._denominador * otra._denominador
-      
-       return Fraccion(nuevo_numerador, nuevo_denominador).simplificada()
-   }   
-
-
-   operator fun minus(otra: Fraccion): Fraccion {
-       val nuevo_numerador = this._numerador * otra._denominador - this._denominador * otra._numerador
-       val nuevo_denominador = this._denominador * otra._denominador
-       return Fraccion(nuevo_numerador, nuevo_denominador).simplificada()
-   }
-
-
-   fun obtenesValor(): String {
-       return toString()
-   }
-
-
-   override fun toString(): String {
-       return "$numerador / $denominador"
-   }
-
-
-   private fun simplificar(): Fraccion {
-       val mcd = mcd(numerador, denominador)
-       return Fraccion(numerador / mcd, denominador / mcd)
-   }
-   private fun simplificada(): Fraccion {
-       return this.simplificar()
-   }
-   private fun mcd(a: Int, b: Int): Int {
-       return if (b == 0) a else mcd(b, a % b)
-   }
+    fun mostrar(){
+        println(this.toString())
+    }
 }
